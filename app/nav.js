36f7463831405bb9cf4f2397e23e385d6a1730cf
v-glb <v.networks@posteo.de>
2019-12-21 22:43:25 +0100

    Implement smooth page transitions with CSS attributes and HTML imports

---
+++ app/nav.js
@@ -0,0 +1,67 @@
+// #################################################################
+//
+//                    INITIAL STATE OF APP
+//
+// #################################################################
+
+const links = document.querySelectorAll('link[rel="import"]')
+
+// Append template to main content on app start
+appendTemplatesToDOM(links);
+
+// Set dashboard as initial page on app start
+let dashboard = document.getElementById('dashboard-section');
+dashboard.classList.add('is-shown');
+
+
+// #################################################################
+//
+//                    HTML IMPORTS HANDLING
+//
+// #################################################################
+
+
+// Import and add each page to the DOM
+function appendTemplatesToDOM(templateLinks) {
+  // Get all template links in index.html
+
+  Array.prototype.forEach.call(templateLinks, (link) => {
+    // Get contents of all templates with class .task-template
+    let template = link.import.querySelector('.task-template');
+    let clone = document.importNode(template.content, true);
+
+    // Append template contents to DOM
+    document.querySelector('.content').appendChild(clone);
+  });
+}
+
+function hideSections() {
+  const sections = document.querySelectorAll('.section-content.is-shown')
+  Array.prototype.forEach.call(sections, (section) => {
+    section.classList.remove('is-shown')
+  });
+}
+
+
+
+
+// #################################################################
+//
+//                      EVENT HANDLERS
+//
+// #################################################################
+
+// TODO: Click events for side menu
+
+document.getElementById('about-page').addEventListener('click', e => {
+
+  hideSections()
+  document.getElementById('about-section').classList.add('is-shown')
+});
+
+
+document.getElementById('dashboard-page').addEventListener('click', e => {
+
+  hideSections()
+  document.getElementById('dashboard-section').classList.add('is-shown')
+});

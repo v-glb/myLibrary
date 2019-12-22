ac4260f77f6b06b517a61aaa491076df7612e315
v-glb <v.networks@posteo.de>
2019-12-22 16:36:55 +0100

    Add menu item highlighting for active page

--- app/nav.js
+++ app/nav.js
@@ -51,8 +51,6 @@ function hideSections() {
 //
 // #################################################################
 
-// TODO: Click events for side menu
-
 document.getElementById('about-page').addEventListener('click', e => {
 
   hideSections()
@@ -64,4 +62,21 @@ document.getElementById('dashboard-page').addEventListener('click', e => {
 
   hideSections()
   document.getElementById('dashboard-section').classList.add('is-shown')
+});
+
+// Highlight active page in side menu
+const menuItems = document.querySelectorAll('.menu-item');
+
+menuItems.forEach((el) => {
+
+  el.addEventListener('click', e => {
+
+    // Remove highlight from previous element
+    let currentActive = document.querySelector('.active');
+    currentActive.classList.remove('active');
+
+    // Add highlight to clicked element
+    el.classList.add('active');
+
+  });
 });
